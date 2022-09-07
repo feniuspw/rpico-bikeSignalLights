db9c93d2e872e0bda111308436d3d52f278a6765
Leonardo Villela Costa <leonardovillela.c@gmail.com>
2022-09-07 14:12:28 -0300

    bike signal lights base code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# rpico-bikeSignalLights
+# Bike Signal Lights
+
+v0.1
+Small project made using Raspberry Pi Pico and MicroPython.
+
+## Basic Components
+* 1x Raspberry Pi Pico
+* 1x 4-8x8 Led Matrix with MAX7219
+* 1x KY-023 Joystick Module (for prototype version)
+* Jumper Cables
+
+## Usage
+Assembly instructions coming soon!
---
+++ main.py
@@ -0,0 +1,89 @@
+from machine import Pin, SPI, ADC
+from time import sleep
+
+from lib import max7219
+
+# KY-023 Joystick Module for testing purposes
+xAxis = ADC(Pin(26))
+yAxis = ADC(Pin(27))
+SW = Pin(22,Pin.IN, Pin.PULL_UP)
+
+# setup LedMatrix 4x8x8
+spi = SPI(0, baudrate=10000000, polarity=1, phase=0, sck=Pin(2), mosi=Pin(3))
+ss = Pin(5, Pin.OUT)
+display = max7219.Matrix8x8(spi, ss, 4)
+display.brightness(1)   # adjust brightness 1 to 15
+
+# clean screen
+display.fill(0)
+display.show()
+
+# Constants
+L_ARROW_STATUS=0
+R_ARROW_STATUS=0
+BRAKE_STATUS=0
+
+def toggle_brake_light():
+    global BRAKE_STATUS
+    BRAKE_STATUS = 0 if BRAKE_STATUS else 1
+    draw_brake_light(BRAKE_STATUS)
+
+def draw_brake_light(color):
+    global display
+    display.fill_rect(10,0,12,8,color)
+    display.pixel(10,0,0)
+    display.pixel(21,0,0)
+    display.pixel(10,7,0)
+    display.pixel(21,7,0)
+
+def toggle_arrow(side):
+    global L_ARROW_STATUS, R_ARROW_STATUS
+    if side == 'l':
+        L_ARROW_STATUS = 0 if L_ARROW_STATUS else 1
+        draw_arrow(L_ARROW_STATUS, side)
+    else:
+        R_ARROW_STATUS = 0 if R_ARROW_STATUS else 1
+        draw_arrow(R_ARROW_STATUS, side)
+
+def draw_arrow(color, side):
+    global display
+    if side == 'l':
+        r = range(0,4)
+        rect_x = 4
+    else:
+        r = reversed(range(28,32))
+        rect_x = 23
+    line_size = 2
+    ypos = 3
+    for xpos in r:
+        display.vline(xpos,ypos,line_size,color)
+        line_size +=2
+        ypos -=1
+    display.fill_rect(rect_x,2,5,4,color)
+
+
+# Main loop
+while True:
+    xRef = xAxis.read_u16()
+    yRef = yAxis.read_u16()
+    brake_button = SW.value()
+
+    # Signal lights
+    if yRef < 20000:
+        toggle_arrow('l')
+        draw_arrow(0,'r') if R_ARROW_STATUS else None
+    elif yRef > 50000:
+        toggle_arrow('r')
+        draw_arrow(0,'l') if L_ARROW_STATUS else None
+    else:
+        draw_arrow(0,'l')
+        draw_arrow(0,'r')
+        
+    # brake (released button default value is 1)
+    if not brake_button:
+        draw_brake_light(1)
+    else:
+        draw_brake_light(0)
+        
+    display.show()
+    sleep(0.4)
